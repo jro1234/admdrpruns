1c3441d3b668496f7e12f07b715489e547e4e2ef
jrossyra <jrossyra@gmail.com>
2017-09-20 16:05:08 -0400

    First commit, untested

---
+++ runmaker.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env/ python
+
+
+
+from __future__ import print_function
+from datetime import datetime
+from subprocess import call
+from shlex import split
+import shutil
+import sys, os
+
+from scripts._argparser import argparser
+
+
+
+if __name__ == "__main__":
+
+    parser = argparser()
+    args = parser.parse_args()
+
+    now = datetime.now()
+
+    run_filename = args.template
+    run_template = 'runpackage/{0}.template'.format(run_filename)
+    print("Reading run script template from:\n", run_template)
+
+    run_name = '{0}'.format(args.project_name)
+    run_folder = 'runs/{0}/'.format(run_name)
+
+    print("Job will be located in directory:\n", run_folder)
+    run_file = run_folder + run_filename
+
+    os.mkdir(run_folder)
+
+    ## TODO args to build run script here
+    run_scripts = ['configuration.cfg']
+    for runscript in run_scripts:
+        shutil.copy('runpackage/'+runscript, run_folder+runscript)
+
+    with open(run_template, 'r') as f_in, open(run_file, 'w') as f_out:
+        text = ''.join([line for line in f_in])
+        f_out.write(text.format(
+            project_name=args.project_name,
+            system_name=args.system_name,
+            threads=args.threads,
+            longts='--longts' if args.longts else '',
+            randomly='-R' if args.randomly else '',
+            strategy=args.strategy,
+            platform=args.platform,
+            n_rounds=args.n_rounds,
+            minlength=args.minlength if args.minlength > args.length else args.length,
+            n_ext=args.n_ext,
+            mk_model= '-M' if args.model else '',
+            n_traj=args.n_traj,
+            prot=args.prot if not args.longts else args.prot * 2 / 5,
+            all=args.all if not args.longts else args.all * 2 / 5,
+            length=args.length,))
+
