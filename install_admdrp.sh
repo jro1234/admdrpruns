45afb7eb2c4cfeb7b0daa46b4ef6fa91751a3a5e
Ossyra, John R <ossyrajr@ornl.gov>
2017-11-13 13:08:38 -0500

    Separated install of task, app stacks

--- install_admdrp.sh
+++ install_admdrp.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+
+# TODO Fix for this, exit won't
+#      work currently because
+#      the mkdir's below will
+#      fail when directory is
+#      already made (some are
+#      duplicates)
+# Exit if anything goes wrong
+#set -e
+
 # If installing on Mac, the echo'd files contain
 # an erroneous '-e'. This is used by to create
 # newlines from the "\n" string, but for some
 # reason is also getting printed to the files.
 
-module load python
+# If you're on Titan (or other system
+# who has python < 2.7.x) you will need
+# to do something like this due to syntax
+# changes.
+#module load python
+
 CWD=`pwd`
 
 # The only check is that the system does (not)
@@ -14,9 +29,19 @@ CWD=`pwd`
 SYSTEM=linux
 #SYSTEM=Mac
 
+# TODO see when this is necessary...
+RP_ENV_PRELOAD=false
+
+# Use these to change what layers are installed
+# if the DB is already installed then nothing
+# will happen even if the INSTALL_DB=true
+INSTALL_DB=true
+INSTALL_APP=true
+INSTALL_TASKS=true
+
 ## Paths for different installer components
 #   - modify however you like...
-PREFIX_ALL=$PROJWORK/bip149/$USER/admdrp/
+PREFIX_ALL=$HOME/admdrp/
 #PREFIX_ALL=$HOME/admdrp/
 
 # Subdirectories to make for runtime data
@@ -101,125 +126,137 @@ if [ ! -d $PREFIX_ALL ]; then
   mkdir -p $PREFIX_ALL
 fi
 
-################################################################################
-#  Install MongoDB                                                             #
-################################################################################
-if [ ! -x "$(command -v mongod)" ]; then
-  mkdir -p $INSTALL_ADMDRP_DB
-  cd $INSTALL_ADMDRP_DB
-  echo "Installing Mongo in: $INSTALL_ADMDRP_DB"
-  curl -O https://fastdl.mongodb.org/$MONGODB_SYSTEM/mongodb-$MONGODB_SYSTEM-x86_64-$MONGODB_VERSION.tgz
-  tar -zxvf mongodb-$MONGODB_SYSTEM-x86_64-$MONGODB_VERSION.tgz
-  mv mongodb-$MONGODB_SYSTEM-x86_64-$MONGODB_VERSION/ $FOLDER_ADMDRP_DB
-  mkdir -p $INSTALL_ADMDRP_DB${FOLDER_ADMDRP_DB}data/db
-  rm mongodb-$MONGODB_SYSTEM-x86_64-$MONGODB_VERSION.tgz
-  echo "# APPENDING PATH VARIABLE with AdaptiveMD Environment" >> ~/.bashrc
-  echo "export ADMDRP_DB=$INSTALL_ADMDRP_DB$FOLDER_ADMDRP_DB" >> ~/.bashrc
-  echo "export PATH=$INSTALL_ADMDRP_DB${FOLDER_ADMDRP_DB}bin:\$PATH" >> ~/.bashrc
-  echo "Done installing Mongo, appended PATH with mongodb bin folder"
-  # Mongo should default to using /tmp/mongo-27017.sock as socket
-  echo -e "net:\n   unixDomainSocket:\n      pathPrefix: ${INSTALL_ADMDRP_DB}${FOLDER_ADMDRP_DB}data/\n   bindIp: 0.0.0.0" > ${INSTALL_ADMDRP_DB}${FOLDER_ADMDRP_DB}/mongo.cfg
-  source ~/.bashrc
-  echo "MongoDB daemon installed here: "
-else
-  echo "Found MongoDB already installed at: "
-fi
-which mongod
-
-################################################################################
-#  Install Env                                                                 #
-################################################################################
-if [ ! -d "$INSTALL_ENV$FOLDER_ADMDRP_ENV" ]; then
-  mkdir -p $INSTALL_ENV
-  cd $INSTALL_ENV
-  virtualenv $INSTALL_ENV$FOLDER_ADMDRP_ENV
-  echo "export ADMDRP_ENV=$INSTALL_ENV$FOLDER_ADMDRP_ENV" >> ~/.bashrc
-  echo "export ADMDRP_ENV_ACTIVATE=\${ADMDRP_ENV}bin/activate" >> ~/.bashrc
-  echo -e "\n# MORE ENVIRONMENT VARIABLES" >> $INSTALL_ENV$FOLDER_ADMDRP_ENV/bin/activate
-  echo "export LD_PRELOAD=/lib64/librt.so.1" >> $INSTALL_ENV$FOLDER_ADMDRP_ENV/bin/activate
-  echo "export RP_ENABLE_OLD_DEFINES=True" >> $INSTALL_ENV$FOLDER_ADMDRP_ENV/bin/activate
-  echo "export RADICAL_PILOT_DBURL='mongodb://rp:rp@ds015335.mlab.com:15335/rp'" >> $INSTALL_ENV$FOLDER_ADMDRP_ENV/bin/activate
-  source ~/.bashrc
-else
-  echo "Found VirtualEnv already installed at: "
-  echo $INSTALL_ENV$FOLDER_ADMDRP_ENV
+if [ $INSTALL_DB = true ]; then
+  ################################################################################
+  #  Install MongoDB                                                             #
+  ################################################################################
+  if [ ! -x "$(command -v mongod)" ]; then
+    mkdir -p $INSTALL_ADMDRP_DB
+    cd $INSTALL_ADMDRP_DB
+    echo "Installing Mongo in: $INSTALL_ADMDRP_DB"
+    curl -O https://fastdl.mongodb.org/$MONGODB_SYSTEM/mongodb-$MONGODB_SYSTEM-x86_64-$MONGODB_VERSION.tgz
+    tar -zxvf mongodb-$MONGODB_SYSTEM-x86_64-$MONGODB_VERSION.tgz
+    mv mongodb-$MONGODB_SYSTEM-x86_64-$MONGODB_VERSION/ $FOLDER_ADMDRP_DB
+    mkdir -p $INSTALL_ADMDRP_DB${FOLDER_ADMDRP_DB}data/db
+    rm mongodb-$MONGODB_SYSTEM-x86_64-$MONGODB_VERSION.tgz
+    echo "# APPENDING PATH VARIABLE with AdaptiveMD Environment" >> ~/.bashrc
+    echo "export ADMDRP_DB=$INSTALL_ADMDRP_DB$FOLDER_ADMDRP_DB" >> ~/.bashrc
+    echo "export PATH=$INSTALL_ADMDRP_DB${FOLDER_ADMDRP_DB}bin:\$PATH" >> ~/.bashrc
+    echo "Done installing Mongo, appended PATH with mongodb bin folder"
+    # Mongo should default to using /tmp/mongo-27017.sock as socket
+    echo -e "net:\n   unixDomainSocket:\n      pathPrefix: ${INSTALL_ADMDRP_DB}${FOLDER_ADMDRP_DB}data/\n   bindIp: 0.0.0.0" > ${INSTALL_ADMDRP_DB}${FOLDER_ADMDRP_DB}/mongo.cfg
+    source ~/.bashrc
+    echo "MongoDB daemon installed here: "
+  else
+    echo "Found MongoDB already installed at: "
+  fi
+  which mongod
 fi
 
-################################################################################
-#  Install AdaptiveMD (and RP via AdaptiveMD package specifications)           #
-################################################################################
-source $ADMDRP_ENV_ACTIVATE
-if [ ! -d "$INSTALL_ADAPTIVEMD/adaptivemd" ]; then
-  mkdir -p $INSTALL_ADAPTIVEMD
-  cd $INSTALL_ADAPTIVEMD
-  git clone https://github.com/$ADAPTIVEMD_VERSION
-  cd adaptivemd
-  git checkout $ADAPTIVEMD_BRANCH
-  pip install $ADMD_APP_PKG
-  pip install $ADAPTIVEMD_INSTALLMETHOD .
-  python -W ignore -c "import adaptivemd" || echo "something wrong with adaptivemd install"
-  echo "if no errors then AdaptiveMD & dependencies installed"
-else
-  echo "Seems AdaptiveMD is already installed, source located here:"
-  echo $INSTALL_ADAPTIVEMD/adaptivemd
-  python -W ignore -c "import adaptivemd; print adaptivemd.__version__"
-fi
-deactivate
+if [ $INSTALL_APP = true ]; then
+  ################################################################################
+  #  Install Env                                                                 #
+  ################################################################################
+  if [ ! -d "$INSTALL_ENV$FOLDER_ADMDRP_ENV" ]; then
+    mkdir -p $INSTALL_ENV
+    cd $INSTALL_ENV
+    virtualenv $INSTALL_ENV$FOLDER_ADMDRP_ENV
+    echo "export ADMDRP_ENV=$INSTALL_ENV$FOLDER_ADMDRP_ENV" >> ~/.bashrc
+    echo "export ADMDRP_ENV_ACTIVATE=\${ADMDRP_ENV}bin/activate" >> ~/.bashrc
+    echo -e "\n# MORE ENVIRONMENT VARIABLES" >> $INSTALL_ENV$FOLDER_ADMDRP_ENV/bin/activate
+    if [ $RP_ENV_PRELOAD = true ]; then
+      echo "ADDING PRELOAD LINE TO ENV ACTIVATE SCRIPT"
+      echo "export LD_PRELOAD=/lib64/librt.so.1" >> $INSTALL_ENV$FOLDER_ADMDRP_ENV/bin/activate
+    fi
+    echo "export RP_ENABLE_OLD_DEFINES=True" >> $INSTALL_ENV$FOLDER_ADMDRP_ENV/bin/activate
+    echo "export RADICAL_PILOT_DBURL='mongodb://rp:rp@ds015335.mlab.com:15335/rp'" >> $INSTALL_ENV$FOLDER_ADMDRP_ENV/bin/activate
+    source ~/.bashrc
+  else
+    echo "Found VirtualEnv already installed at: "
+    echo $INSTALL_ENV$FOLDER_ADMDRP_ENV
+  fi
 
-################################################################################
-#  Install Miniconda                                                           #
-################################################################################
-if [ -z ${CONDAPATH+x} ]; then
-  mkdir -p $INSTALL_CONDA
-  cd $INSTALL_CONDA
-  curl -O https://repo.continuum.io/miniconda/Miniconda$CONDA_VERSION-latest-Linux-x86_64.sh
-  bash Miniconda$CONDA_VERSION-latest-Linux-x86_64.sh -p ${INSTALL_CONDA}miniconda$CONDA_VERSION/
-  echo "Miniconda conda executable here: "
-  echo "export CONDAPATH=${INSTALL_CONDA}miniconda$CONDA_VERSION/bin/" >> ~/.bashrc
-  source ~/.bashrc
-  PATH=$CONDAPATH:$PATH
-  conda config --append channels conda-forge
-  conda config --append channels omnia
-  conda install conda=$CONDA_PKG_VERSION
-  rm Miniconda$CONDA_VERSION-latest-Linux-x86_64.sh
-else
-  echo "Conda is already installed, binaries folder located here:"
-  PATH=$CONDAPATH:$PATH
-  echo $CONDAPATH
+  ################################################################################
+  #  Install AdaptiveMD (and RP via AdaptiveMD package specifications)           #
+  ################################################################################
+  source $ADMDRP_ENV_ACTIVATE
+  if [ ! -d "$INSTALL_ADAPTIVEMD/adaptivemd" ]; then
+    mkdir -p $INSTALL_ADAPTIVEMD
+    cd $INSTALL_ADAPTIVEMD
+    git clone https://github.com/$ADAPTIVEMD_VERSION
+    cd adaptivemd
+    git checkout $ADAPTIVEMD_BRANCH
+    pip install $ADMD_APP_PKG
+    pip install $ADAPTIVEMD_INSTALLMETHOD .
+    python -W ignore -c "import adaptivemd" || echo "something wrong with adaptivemd install"
+    echo "if no errors then AdaptiveMD & dependencies installed"
+  else
+    echo "Seems AdaptiveMD is already installed, source located here:"
+    echo $INSTALL_ADAPTIVEMD/adaptivemd
+    python -W ignore -c "import adaptivemd; print adaptivemd.__version__"
+  fi
+  deactivate
+
+  ################################################################################
+  #   Copy the running scripts over and make data directory                      #
+  ################################################################################
+  mkdir -p $INSTALL_ADMDRP_DATA
+  mkdir -p $INSTALL_ADMDRP_RUNS
+  cp -r $CWD/runs/ $INSTALL_ADMDRP_RUNS
+  
+  echo "export ADMDRP_DATA=$INSTALL_ADMDRP_DATA" >> ~/.bashrc
+  echo "export ADMDRP_RUNS=${INSTALL_ADMDRP_RUNS}runs/" >> ~/.bashrc
 fi
 
-################################################################################
-#  Install Conda Environment for AdaptiveMD                                    #
-################################################################################
-if [[ ! -z "$CONDA_ENV_NAME" ]]; then
-  ENVS=`conda env list`
-  if ! echo "$ENVS" | grep -q "$CONDA_ENV_NAME"; then
-    echo "Creating and Activating new conda env: $CONDA_ENV_NAME"
-    conda create -n $CONDA_ENV_NAME python=$CONDA_ENV_PYTHON
-################################################################################
-#   Install AdaptiveMD Task Stack in Conda Environment                         #
-################################################################################
-    source $CONDAPATH/activate $CONDA_ENV_NAME
-    echo "Installing these Packages in AdaptiveMD Task Layer"
-    echo $ADMD_TASK_PKG
-    conda install $ADMD_TASK_PKG
-    source deactivate
+
+if [ $INSTALL_TASKS = true ]; then
+  ################################################################################
+  #  Install Miniconda                                                           #
+  ################################################################################
+  if [ -z ${CONDAPATH+x} ]; then
+    mkdir -p $INSTALL_CONDA
+    cd $INSTALL_CONDA
+    curl -O https://repo.continuum.io/miniconda/Miniconda$CONDA_VERSION-latest-Linux-x86_64.sh
+    bash Miniconda$CONDA_VERSION-latest-Linux-x86_64.sh -p ${INSTALL_CONDA}miniconda$CONDA_VERSION/
+    echo "Miniconda conda executable here: "
+    echo "export CONDAPATH=${INSTALL_CONDA}miniconda$CONDA_VERSION/bin/" >> ~/.bashrc
+    source ~/.bashrc
+    PATH=$CONDAPATH:$PATH
+    conda config --append channels conda-forge
+    conda config --append channels omnia
+    conda install conda=$CONDA_PKG_VERSION
+    rm Miniconda$CONDA_VERSION-latest-Linux-x86_64.sh
+  else
+    echo "Conda is already installed, binaries folder located here:"
+    PATH=$CONDAPATH:$PATH
+    echo $CONDAPATH
+  fi
+  
+  ################################################################################
+  #  Install Conda Environment for AdaptiveMD                                    #
+  ################################################################################
+  if [[ ! -z "$CONDA_ENV_NAME" ]]; then
+    ENVS=`conda env list`
+    if ! echo "$ENVS" | grep -q "$CONDA_ENV_NAME"; then
+      echo "Creating and Activating new conda env: $CONDA_ENV_NAME"
+      conda create -n $CONDA_ENV_NAME python=$CONDA_ENV_PYTHON
+  ################################################################################
+  #   Install AdaptiveMD Task Stack in Conda Environment                         #
+  ################################################################################
+      source $CONDAPATH/activate $CONDA_ENV_NAME
+      echo "Installing these Packages in AdaptiveMD Task Layer"
+      echo $ADMD_TASK_PKG
+      conda install $ADMD_TASK_PKG
+      source deactivate
+    fi
   fi
 fi
 
-################################################################################
-#   Copy the running scripts over and make data directory                      #
-################################################################################
-mkdir -p $INSTALL_ADMDRP_DATA
-mkdir -p $INSTALL_ADMDRP_RUNS
-cp -r $CWD/runs/ $INSTALL_ADMDRP_RUNS
-
-echo "export ADMDRP_DATA=$INSTALL_ADMDRP_DATA" >> ~/.bashrc
-echo "export ADMDRP_RUNS=${INSTALL_ADMDRP_RUNS}runs/" >> ~/.bashrc
-
+  
 ################################################################################
 #  Finalize & Cleanup                                                          #
 ################################################################################
 cd $CWD
-module unload python/2.7.9
+
+#module unload python/2.7.9
 
