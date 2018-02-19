4ecf572f5291811cb8eda067ef1b4685044029b1
jrossyra <jrossyra@gmail.com>
2018-02-19 14:58:28 -0500

    [OpenMM] automated install, add openmmlib to lib path

--- install_usereqs.sh
+++ install_usereqs.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
+
+
+# Nothing will work if this is False
+# or these aren't already given by 
+# a prior installation
+### For each install
+APPEND_BASHRC=True
+
+### Only on first install
+ADD_ENVIRONMENT=False
+
+#DEBUG
+#TIMESTAMPS
+
 CWD=`pwd`
+ENV_BASE=ADMDRP
+ENV_HOME=`echo $ENV_BASE | tr '[:upper:]' '[:lower:]'`
 
-ENV_BASE=ADMDRP3
-
-INSTALL_HOME=$PROJWORK/bip149/$USER/admdrp3/
+INSTALL_HOME=$PROJWORK/bip149/$USER/$ENV_HOME/
 FOLDER_ADMDRP_ENV=admdrpenv/
 FOLDER_ADMDRP=admdrp/
 
 ADAPTIVEMD_PKG=jrossyra/adaptivemd.git
 ADAPTIVEMD_BRANCH=rp_integration
-
+RCT_DEDICATED_ADAPTIVEMD_BRANCH=project/adaptivemd
 RUTILS_PKG=radical-cybertools/radical.utils.git
-RUTILS_BRANCH=
-
+RUTILS_BRANCH=$RCT_DEDICATED_ADAPTIVEMD_BRANCH
 SAGA_PKG=radical-cybertools/saga-python.git
-SAGA_BRANCH=fix/issue_1538
-
+SAGA_BRANCH=$RCT_DEDICATED_ADAPTIVEMD_BRANCH
 RPILOT_PKG=radical-cybertools/radical.pilot.git
-RPILOT_BRANCH=fix/issue_1538
+RPILOT_BRANCH=$RCT_DEDICATED_ADAPTIVEMD_BRANCH
 
 # User must download OpenMM-Linux precompiled binaries
 # and untar it. This just tells the script where these
@@ -28,13 +40,9 @@ RPILOT_BRANCH=fix/issue_1538
 OPENMM_LOC=$PROJWORK/bip149/$USER/
 FOLDER_OPENMM=OpenMM-7.0.1-Linux
 
-# Nothing will work if this is False
-# or these aren't already given by 
-# a prior installation
-APPEND_BASHRC=True
-ADD_ENVIRONMENT=True
-#DEBUG
-#TIMESTAMPS
+OPENMM_LIBRARY_PREFIX=lib/
+OPENMM_PLUGIN_PREFIX=lib/plugins/
+OPENMM_INSTALL_LOC=$INSTALL_HOME$FOLDER_ADMDRP/openmm/
 
 
 module load python
@@ -66,6 +74,7 @@ source ~/.bashrc
 #eval echo \$${ENV_BASE}_ENV_ACTIVATE
 eval source \$${ENV_BASE}_ENV_ACTIVATE
 
+# DEPENDENCY INSTALL
 cat requirements.txt | xargs -n 1 -L 1 pip install
 
 cd $INSTALL_HOME
@@ -79,32 +88,48 @@ else
   cd $FOLDER_ADMDRP
 fi
 
-
+# RADICAL UTILS INSTALL
 git clone https://github.com/$RUTILS_PKG
 cd radical.utils
 git checkout $RUTILS_BRANCH
 pip install .
 cd ../
 
+# SAGA PYTHON INSTALL
 git clone https://github.com/$SAGA_PKG
 cd saga-python
 git checkout $SAGA_BRANCH
 pip install .
 cd ../
 
+# RADICAL PILOT INSTALL
 git clone https://github.com/$RPILOT_PKG
 cd radical.pilot
 git checkout $RPILOT_BRANCH
 pip install .
 cd ../
 
+# ADAPTIVEMD INSTALL
 git clone https://github.com/$ADAPTIVEMD_PKG
 cd adaptivemd
 git checkout $ADAPTIVEMD_BRANCH
 pip install .
 
+# OPENMM INSTALL
 cd $OPENMM_LOC$FOLDER_OPENMM
 module load cudatoolkit
-sh install.sh
+pwd
+ls -grth
+expect -c "
+    spawn sh install.sh
+    expect "Enter?install?location*"
+    send  \"$OPENMM_INSTALL_LOC\r\"
+    expect "Enter?path?to?Python*"
+    send  \"\r\"
+    expect eof
+    "
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OPENMM_INSTALL_LOC$OPENMM_PLUGIN_PREFIX" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OPENMM_INSTALL_LOC$OPENMM_LIBRARY_PREFIX" >> ~/.bashrc
 
 cd $CWD
+
