a2107bcc0d7824d9eda98e7383ab0a491aeb9460
jrossyra <jrossyra@gmail.com>
2018-03-18 14:01:48 -0400

    Updated install with mongod, env variables

--- install_usereqs.sh
+++ install_usereqs.sh
@@ -2,14 +2,54 @@
 
 
 
+# these ones saved to environment variables
+INSTALL_ADMD_DB=$PROJWORK/bip149/$USER/
+# Folder names for our installed components
+FOLDER_ADMD_DB=mongodb
+MONGODB_VERSION=3.3.0
+###############################################################################
+#  Install MongoDB                                                            #
+###############################################################################
+if [ ! -x "$(command -v mongod)" ]; then
+  cd $INSTALL_ADMD_DB
+  echo "Installing Mongo in: $INSTALL_ADMD_DB"
+  curl -O https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-$MONGODB_VERSION.tgz
+  tar -zxvf mongodb-linux-x86_64-$MONGODB_VERSION.tgz
+  mkdir mongodb
+  mv mongodb-linux-x86_64-$MONGODB_VERSION/ $FOLDER_ADMD_DB
+  rm mongodb-linux-x86_64-$MONGODB_VERSION.tgz
+  echo "# APPENDING PATH VARIABLE with AdaptiveMD Environment" >> ~/.bashrc
+  echo "export ADMD_DB=${INSTALL_ADMD_DB}${FOLDER_ADMD_DB}/" >> ~/.bashrc
+  echo "export PATH=${INSTALL_ADMD_DB}${FOLDER_ADMD_DB}/mongodb-linux-x86_64-$MONGODB_VERSION/bin/:\$PATH" >> ~/.bashrc
+  echo "export LOGIN_HOSTNAME=`ip addr show bond0 | grep -Eo '(addr:)?([0-9]*\.){3}[0-9]*' | head -n1`" >> ~/.bashrc
+  echo "# NOTE These should be overwritten according to the"
+  echo "#      actual `mongod` instance host ip. Using the"
+  echo "#      launched-method of workflow, these will be"
+  echo "#      configured automatically at runtime and"
+  echo "#      overwrite with correct locations."
+  echo "export RADICAL_PILOT_DBURL=\"mongodb://\${LOGIN_HOSTNAME}:27777/rp\""
+  echo "export ADMD_DBURL=\"mongodb://\${LOGIN_HOSTNAME}:27017/\""
+  echo "Done installing Mongo, appended PATH with mongodb bin folder"
+  source ~/.bashrc
+  echo "MongoDB daemon installed here: "
+else
+  echo "Found MongoDB already installed at: "
+fi
+which mongod
+
+
+
+
+
 # Nothing will work if this is False
 # or these aren't already given by 
 # a prior installation
 ### For each install
 APPEND_BASHRC=True
 
-### Only on first install
-ADD_ENVIRONMENT=False
+# Turn this off to prevent env copies
+# appended to bashrc with reinstalls
+ADD_ENVIRONMENT=True
 
 #DEBUG
 #TIMESTAMPS
@@ -51,13 +91,24 @@ virtualenv $INSTALL_HOME$FOLDER_ADMDRP_ENV
 if [ $ADD_ENVIRONMENT = True ]
 then
   #echo "Appending virtualenv activate script"
-  echo "Appending bashrc with RP Environment Vars for DEBUG"
   #RP_VARSLOC=$INSTALL_HOME$FOLDER_ADMDRP_ENV/bin/activate
+  echo "Appending bashrc with RP Environment Vars for DEBUG"
   RP_VARSLOC=~/.bashrc
-  echo -e "\n# MORE ENVIRONMENT VARIABLES" >> $RP_VARSLOC
+
+  echo "# This is home to the execution working directories" >> $RP_VARSLOC
+  echo "# ie the Radical Pilot Sandbox" >> $RP_VARSLOC
+  echo "export RADICAL_SANDBOX=$MEMBERWORK/bip149/radical.pilot.sandbox/" >> $RP_VARSLOC
+
+  echo "#ENVIRONMENT VARIABLES to control output" >> $RP_VARSLOC
+  echo "#export RADICAL_PILOT_PROFILE=\"True\"" >> $RP_VARSLOC
+  echo "#export RADICAL_PROFILE=\"True\"" >> $RP_VARSLOC
+  echo "export ADMD_PROFILE=\"INFO\"" >> $RP_VARSLOC
+  echo "# These ones are for RP DEBUG verbosity" >> $RP_VARSLOC
+  echo "#export RADICAL_SAGA_PTY_VERBOSE=\"DEBUG\"" >> $RP_VARSLOC
+  echo "#export RADICAL_VERBOSE=\"DEBUG\"" >> $RP_VARSLOC
+  echo -e "\n# REQUIRED ENVIRONMENT VARIABLES" >> $RP_VARSLOC
   echo "export LD_PRELOAD=/lib64/librt.so.1" >> $RP_VARSLOC
   echo "export RP_ENABLE_OLD_DEFINES=True" >> $RP_VARSLOC
-  echo "export RADICAL_PILOT_DBURL='mongodb://rp:rp@ds015335.mlab.com:15335/rp'" >> $RP_VARSLOC
 fi
 
 if [ $APPEND_BASHRC = True ]
@@ -68,6 +119,10 @@ then
   echo "export ${ENV_BASE}_RUNS=$INSTALL_HOME${FOLDER_ADMDRP}runs/" >> ~/.bashrc
   echo "export ${ENV_BASE}_ADAPTIVEMD=$INSTALL_HOME${FOLDER_ADMDRP}adaptivemd/" >> ~/.bashrc
   echo "export ${ENV_BASE}_DATA=$INSTALL_HOME$FOLDER_ADMDRP" >> ~/.bashrc
+  echo "FOR YOUR WORKFLOW TO RUN PROPERLY, UNCOMMENT THIS"
+  echo "LINE IN YOUR .bashrc FILE"
+  echo "source \$ADMDRP_ENV_ACTIVATE"
+  echo "source \$ADMDRP_ENV_ACTIVATE" >> ~/.bashrc
 fi
 
 source ~/.bashrc
@@ -128,8 +183,8 @@ expect -c "
     send  \"\r\"
     expect eof
     "
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OPENMM_INSTALL_LOC$OPENMM_PLUGIN_PREFIX" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$OPENMM_INSTALL_LOC$OPENMM_LIBRARY_PREFIX" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$OPENMM_INSTALL_LOC$OPENMM_PLUGIN_PREFIX" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$OPENMM_INSTALL_LOC$OPENMM_LIBRARY_PREFIX" >> ~/.bashrc
 
 cd $CWD
 
